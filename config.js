3900ba209adcb23f8b9f1f7d6829d3b744b1704c
Subin Siby <subins2000@gmail.com>
2018-08-17 15:31:38 +0530

    Make config file .js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "name": "OpenSalve",
+  "version": "0.1",
+  "db": {
+    "client": "pg",
+    "connection": {
+      "host": "localhost",
+      "port": "5432",
+      "username": "username",
+      "password": "password",
+      "database": "opensalve"
+    }
+  }
+}
