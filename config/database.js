d80d300dce2458fecb87991b5eb997627606ab3e
Subin Siby <subins2000@gmail.com>
2018-08-18 22:15:30 +0530

    Separated config files to a folder

---
+++ config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "client": "pg",
+  "connection": {
+    "host": "localhost",
+    "port": "5432",
+    "username": "username",
+    "password": "password",
+    "database": "opensalve"
+  }
+}
