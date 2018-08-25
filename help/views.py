c9024cdf2a3ede2b69a2c571a53da289c8b6f6d9
Subin Siby <subins2000@gmail.com>
2018-08-26 01:37:13 +0530

    Add view for adding request

--- help/views.py
+++ help/views.py
@@ -1,1 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+
+from help.serializers import RequestsSerializer
+
+
+class AddRequest(generics.CreateAPIView):
+    serializer_class = RequestsSerializer
