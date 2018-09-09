ef4eb23662842c688de049eb04140778b85ea6de
Subin Siby <subins2000@gmail.com>
2018-09-09 17:53:01 +0530

    Use drf_yasg

--- opensalve/settings.py
+++ opensalve/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_swagger',
+    'drf_yasg',
     'accounts',
     'help',
 ]
