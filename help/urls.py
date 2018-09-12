bf0bdd465fee4591af9ad6512f39495963aa6373
Subin Siby <subins2000@gmail.com>
2018-09-12 23:47:32 +0530

    Add help/r/<id>/status to get/update status info

--- help/urls.py
+++ help/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add', views.AddRequest.as_view()),
     # r/ Request
     path('r/<int:id>', views.ViewRequest.as_view()),
+    path('r/<int:id>/status', views.StatusRequest.as_view()),
 ]
