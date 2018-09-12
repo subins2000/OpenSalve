a0fcc3ed73c3ef086716406add5dc897d96b73c7
Subin Siby <subins2000@gmail.com>
2018-09-13 00:00:00 +0530

    Add custom IsVolunteer permission class

---
+++ help/permissions.py
@@ -0,0 +1,17 @@
+from accounts.models import Users
+from rest_framework import permissions
+
+
+class IsVolunteer(permissions.BasePermission):
+    """Custom permission to only allow volunteers to edit requests
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        user = Users.objects.get(username=request.user.username)
+
+        return user.role == 'volunteer'
