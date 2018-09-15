87eb5b08a95a8717eadef39967c996fec9a68e85
Subin Siby <subins2000@gmail.com>
2018-09-15 23:17:46 +0530

    Custom permission to edit user details. Admin can edit all, but only account holders can change their own info

---
+++ accounts/permissions.py
@@ -0,0 +1,18 @@
+from accounts.models import Users
+from rest_framework import permissions
+
+
+class IsEditable(permissions.BasePermission):
+    """Only allow user and admin to edit their info
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        editing_username = view.kwargs.get(view.lookup_url_kwarg)
+        user = Users.objects.get(username=request.user.username)
+
+        return user.role == 'admin' or editing_username == user.username
