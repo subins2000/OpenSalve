69162e608f5d45113dc83bb099c16810aa72c4ff
Subin Siby <subins2000@gmail.com>
2018-09-26 18:10:55 +0530

    Add permission to use phone as authenticator for editing requests

--- help/permissions.py
+++ help/permissions.py
@@ -1,4 +1,7 @@
+import json
+
 from accounts.models import User
+from help.models import Requests
 from rest_framework import permissions
 
 
@@ -15,3 +18,19 @@ class IsVolunteer(permissions.BasePermission):
         user = User.objects.get(username=request.user.username)
 
         return user.role == 'volunteer' or user.role == 'admin'
+
+
+class IsPhoneAuthenticated(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        editing_id = view.kwargs.get(view.lookup_url_kwarg)
+        editing_request = Requests.objects.get(id=editing_id)
+
+        request_data = json.loads(request.body.decode('utf-8'))
+
+        return editing_request.phone == request_data['phone']
