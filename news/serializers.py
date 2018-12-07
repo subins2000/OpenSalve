f037d859625a3d6052d39d16c385a86ee807d684
Subin Siby <subins2000@gmail.com>
2018-12-08 00:34:43 +0530

    Set user object value

--- news/serializers.py
+++ news/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 
+from accounts.models import User
 from news.models import News
 
 
 class NewsSerializer(serializers.ModelSerializer):
 
+    user = serializers.SerializerMethodField()
+
     class Meta:
 
         model = News
         fields = '__all__'
+
+    def validate_user(self, value):
+        return self.context['request'].user
+
+    def get_user(self, news):
+        return self.context['request'].user.username
